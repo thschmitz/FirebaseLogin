5a55f2e4ebae8ab03efde9f8ec986837a9d12844
Thomas Henrique Schmitz <thomas.henrique.schmitz@gmail.com>
2022-01-13 14:48:19 -0300

    Desenvolvimento

---
+++ src/Api.js
@@ -0,0 +1,10 @@
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+import firebaseConfig from "./firebaseConfig"
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebaseApp.auth()
+
+export {db, auth}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+export default {
+    apiKey: "AIzaSyAQYtMwTxp5YtJzfNZc4KTxpQx5J4Jpk9A",
+
+    authDomain: "cadastrofirebase-2338b.firebaseapp.com",
+  
+    projectId: "cadastrofirebase-2338b",
+  
+    storageBucket: "cadastrofirebase-2338b.appspot.com",
+  
+    messagingSenderId: "381648422352",
+  
+    appId: "1:381648422352:web:ec1727c5d707e9f9eee5d5",
+  
+    measurementId: "G-L08DWDMY8W"
+  
+}
